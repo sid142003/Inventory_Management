e1e7390fd1edcd5d37c47ee90b3afd4f60cdecf4
sid142003 <97834759+sid142003@users.noreply.github.com>
2023-04-05 22:11:38 +0530

    one

--- src/db/db.js
+++ src/db/db.js
@@ -1,5 +1,9 @@
 const mongoose=require("mongoose")
-mongoose.connect("mongodb+srv://siddhant:%40Siddhant14022003@cluster0.khebxji.mongodb.net/Inventory_Information?retryWrites=true&w=majority")
+mongoose.connect("mongodb+srv://siddhant:%40Siddhant14022003@cluster0.khebxji.mongodb.net/Inventory_Information?retryWrites=true&w=majority", {
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+   
+})
 .then(()=>{
     console.log("connected");
 }).catch((error)=>{
