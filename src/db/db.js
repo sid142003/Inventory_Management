63c7fb357c49ddc1bbb01302350a5bd0b9926164
sid142003 <97834759+sid142003@users.noreply.github.com>
2023-04-05 22:06:26 +0530

    update3

--- src/db/db.js
+++ src/db/db.js
@@ -1,5 +1,5 @@
 const mongoose=require("mongoose")
-mongoose.connect("mongodb://127.0.0.1:27017/form")
+mongoose.connect("mongodb+srv://siddhant:%40Siddhant14022003@cluster0.khebxji.mongodb.net/Inventory_Information?retryWrites=true&w=majority")
 .then(()=>{
     console.log("connected");
 }).catch((error)=>{
